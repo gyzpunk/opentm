185fdb522632545b8ca6b622125b18d9235fb89f
gyzpunk <florent@captier.org>
2014-10-20 09:52:32 +0200

    Add the admin part

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
+from core.models import Assignation, Task
 
-# Register your models here.
+
+class AssignationAdminInline(admin.TabularInline):
+    model = Assignation
+    extra = 1
+
+
+class TaskAdmin(admin.ModelAdmin):
+    model = Task
+    inlines = (AssignationAdminInline,)
+
+
+class AssignationAdmin(admin.ModelAdmin):
+    model = Assignation
+    list_display = ('pk', 'task', 'user', 'year', 'month', 'week')
+
+admin.site.register(Task, TaskAdmin)
+admin.site.register(Assignation, AssignationAdmin)
--- core/models.py
+++ core/models.py
@@ -1,14 +1,40 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+from datetime import date, timedelta
 
 
 class Task(models.Model):
     name = models.CharField(max_length=255, unique=True, null=False, blank=False)
-    users = models.ManyToManyField(User, through=Assignation)
+    users = models.ManyToManyField(User, through='Assignation', related_name='tasks')
+
+    def __str__(self):
+        return self.name
 
 
 class Assignation(models.Model):
-    user = models.ForeignKey(User)
-    task = models.ForeignKey(Task)
-    wkld_planned = models.PositiveSmallIntegerField()
+    user = models.ForeignKey(User, related_name='assignations')
+    task = models.ForeignKey(Task, related_name='assignations')
+    week = models.PositiveSmallIntegerField(max_length=55, verbose_name='Week number')
+    year = models.PositiveIntegerField(verbose_name='Year')
+    wkld_planned = models.PositiveSmallIntegerField(max_length=5, verbose_name='Planned number of days')
+    wkld_current = models.PositiveSmallIntegerField(max_length=5, verbose_name='Current number of days')
+
+    @property
+    def isocalendar(self):
+        return (self.year, self.week, 1)
+
+    @property
+    def month(self):
+        """
+        Returns the month of the assignation based on it's week and year.
+        :return: Month of year
+        :rtype: int
+        """
+        return (date(self.year, 1, 1) + timedelta(weeks=self.week-1)).month
+
+    def __str__(self):
+        return '%s on %s (%i-%i)' % (self.user, self.task, self.year, self.week)
+
+    class Meta:
+        unique_together = (('user', 'task', 'year', 'week'),)
